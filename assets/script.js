b3e7b6c4763a8aed2aff4069845beaf2c475276c
Mateus <mateusvaldejao@Mateuss-MacBook-Pro.local>
2022-06-25 17:05:21 -0400

    initial files and declarations

---
+++ assets/script.js
@@ -0,0 +1,37 @@
+var questions = ["Commonly used data types DO NOT include:", "The condition in an if/else statement is inclosed within ____.", "Arrays in JavaScript can be used to store ___.", "String values must be enclosed within ___ when being assigned to variables.", "A very useful tool used during development and debugging for printing content to the debugger is:"];
+
+questionCounter = 0;
+
+var questionsObject = [{
+    questionId: 0,
+    question: questions[questionCounter],
+    answers: ["strings", "booleans", "alerts", "numbers"],
+    correctAnswer: answers[2]
+    },
+    {
+    questionId: 1,
+    question: questions[questionCounter],
+    answers: ["quotes", "curly brackets", "parentheses", "square brackets"],
+    correctAnswer: answers[2] 
+    },
+    {
+    questionId: 2,
+    question: questions[questionCouter],
+    answers: ["numbers and strings", "other arrays", "booleans", "all of the above"],              
+    correctAnswer: answers[3]
+    },
+    { 
+    questionId: 3,
+    question: questions[questionCounter],
+    answers: ["commas", "curly brackets", "quotes", "parentheses"],
+    correctAnswer: answers[2]
+    },
+    {
+    questionId: 4,
+    question: questions[questionCounter],
+    answers: ["JavaScript", "terminal/bash", "for loops", "console.log"],
+    correctAnswer: answers[3]
+    },  
+]
+
+
