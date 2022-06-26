fbdfb233e9d13f5a2804ae687d1ca0d71def3cc1
Mateus <mateusvaldejao@Mateuss-MacBook-Pro.local>
2022-06-25 22:29:53 -0400

    code checks answer

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,5 @@
 // create startButtonEl to allow listening of button click to start game
-var startButtonEl = document.querySelector(".btn");
+var startButtonEl = document.querySelector("#start-btn");
 var mainEl = document.querySelector("main");
 var timeEl = document.querySelector("time");
 
@@ -10,34 +10,29 @@ questionCounter = 0;
 intialTime = 75;
 
 var qsObjArray = [{
-    questionId: 0,
-    question: questions[questionCounter],
+    question: questions[0],
     answers: ["strings", "booleans", "alerts", "numbers"],
-    correctAnswer: "alerts"
+    correctAnswer: "3. alerts"
     },
     {
-    questionId: 1,
-    question: questions[questionCounter],
+    question: questions[1],
     answers: ["quotes", "curly brackets", "parentheses", "square brackets"],
-    correctAnswer: "parentheses" 
+    correctAnswer: "3. parentheses" 
     },
     {
-    questionId: 2,
-    question: questions[questionCounter],
+    question: questions[2],
     answers: ["numbers and strings", "other arrays", "booleans", "all of the above"],              
-    correctAnswer: "all of the above"
+    correctAnswer: "4. all of the above"
     },
     { 
-    questionId: 3,
-    question: questions[questionCounter],
+    question: questions[3],
     answers: ["commas", "curly brackets", "quotes", "parentheses"],
-    correctAnswer: "quotes"
+    correctAnswer: "3. quotes"
     },
     {
-    questionId: 4,
-    question: questions[questionCounter],
+    question: questions[4],
     answers: ["JavaScript", "terminal/bash", "for loops", "console.log"],
-    correctAnswer: "console.log"
+    correctAnswer: "4. console.log"
     },  
 ]
 
@@ -45,6 +40,7 @@ var qsObjArray = [{
 var startGame = function (event) {
     var h1El = document.querySelector(".head-quiz");
     var pEl = document.querySelector(".p-quiz");
+    
 
     // take header, p, and button and remove from page
     h1El.remove();
@@ -55,24 +51,66 @@ var startGame = function (event) {
     questionDisplay(questionCounter);
 }
 
-var questionDisplay = function (questionCount) {
-    countdown();
+var clear = function () {
+    var h3El = document.querySelector("h3");
 
-    // displays question for user
-    h3El = document.createElement("h3");
-    h3El.textContent = qsObjArray[questionCounter].question;
-    mainEl.appendChild(h3El);
-    // changes align item property to align all main child elements
-    mainEl.style.alignItems = "flex-start";
+    var buttonHolderEl = document.querySelector(".button-holder");
+
+    h3El.remove();
+    buttonHolderEl.remove();
+}
+
+var questionDisplay = function (questionCount) {
+  countdown();
+
+  // displays question for user
+  h3El = document.createElement("h3") 
+  h3El.textContent = qsObjArray[questionCounter].question;
+  buttonHolderEl = document.createElement("div");
+  buttonHolderEl.className = "button-holder";
+  mainEl.appendChild(h3El);
+  mainEl.appendChild(buttonHolderEl);
+  // changes align item property to align all main child elements
+  mainEl.style.alignItems = "flex-start";
 
   // create four buttons as answer options
-    for (var i = 0; i < qsObjArray[questionCount].answers.length; i++) {
-        var answerButtonEl = document.createElement("button");
-        answerButtonEl.className = "btn";
-        answerButtonEl.textContent = (i + 1) + ". " + qsObjArray[questionCount].answers[i];
-        // appends to main element
-        mainEl.appendChild(answerButtonEl);
+  for (var i = 0; i < qsObjArray[questionCount].answers.length; i++) {
+    var answerButtonEl = document.createElement("button");
+    answerButtonEl.className = "btn";
+    answerButtonEl.setAttribute("id", "answer-btn");
+    answerButtonEl.textContent =
+      i + 1 + ". " + qsObjArray[questionCount].answers[i];
+    // appends to main element
+    buttonHolderEl.appendChild(answerButtonEl);
   }
+
+  // create event listener for answer click
+  mainEl.addEventListener("click", whichButton);
+}
+
+var whichButton = function (event) {
+    // checks which button option was clicked
+    if (event.target.matches("#answer-btn")) {
+        ansButtonEl = event.target;
+        answerChecker(ansButtonEl);
+    }
+}
+
+var answerChecker = function (button) {
+  if (button.textContent === qsObjArray[questionCounter].correctAnswer) {
+    var answerDisplayEl = document. createElement("section");
+    answerDisplayEl.className = "answer-display";
+    answerDisplayEl.textContent = "Correct!";
+    mainEl.appendChild(answerDisplayEl);
+  } else {
+    var answerDisplayEl = document.createElement("section");
+    answerDisplayEl.className = "answer-display";
+    answerDisplayEl.textContent = "Wrong!";
+    mainEl.appendChild(answerDisplayEl);
+  }
+  questionCounter++;
+  clear();
+  questionDisplay(questionCounter);
 }
 
 var countdown = function () {
@@ -81,6 +119,5 @@ var countdown = function () {
     intialTime--;
 }, 1000)
 }
-
 
 startButtonEl.addEventListener("click", startGame);
