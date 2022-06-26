468ef3b29eecb2fa1104235003209e404bd6ee62
Mateus <mateusvaldejao@Mateuss-MacBook-Pro.local>
2022-06-26 13:50:27 -0400

    score are extracted in order in highscores page

--- highscore.js
+++ highscore.js
@@ -21,7 +21,30 @@ var highscoreRetrieve = function () {
   for (var i = 0; i < savedHighscores.length; i++) {
     highScores.push(savedHighscores[i]);
   }
+
+    orderScores();
 };
+
+// orders score from best to worst
+var orderScores = function () {
+    // loop through highScores and take score value, compare to previous extracted 
+    scoreStart = -9999;
+    theScore = []
+    for (var i = 0; i < highScores.length; i++) {
+        var space = highScores[i].indexOf(" ");
+        score = (highScores[i].slice(space + 1));
+        if (parseInt(score) > scoreStart) {
+            scoreStart = score
+            theScore.unshift(score);
+        }
+        else if (parseInt(score) <= scoreStart) {
+            theScore.push(score);
+        }
+    }
+    
+
+}
+
 
 // when clicked, clear highscores
 clearButtonEl.addEventListener("click", clearHistory);
